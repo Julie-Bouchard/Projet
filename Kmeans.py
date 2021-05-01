f15e8ab242dade418721e42cf31b2cbea1fe40be
Grarat Maroine <mgrarat@gmail.com>
2021-05-01 20:59:01 +0200

    maj

--- Kmeans.py
+++ Kmeans.py
@@ -15,22 +15,24 @@ class Kmeans:
         #Disons une matrice de taille n,p. Il y'a donc n individus et p variables.
         #La première etape consite à initialiser l'algorithme et désigner des centres au hasard
         #On selectionne les k premiers individus de la table.
-        L=[] #La liste des centres mobiles à l'étape n
+        L=[] #La liste des centres mobiles à l'étape 0
         for k in range (0, self.nombre_classe):
             L.append([self[k]])
+        M=L
         #Une fois que l'initialisation est faite il faut faire bouger les centres jusqu'à avoir une partition stable.
         while '''norme(S[k])>10**(-2)''':
+            N=M #La liste des centres mobiles à l'étape n
             M=[] #La liste des centres mobiles à l'étape n+1
             S=[] #La liste des différence entre l'étape n et n+1
             #On partitionne maintenant notre table
             #On calcule toute les distances
-            for individu in L :
+            for individu in self :
                 T=[]
-                for centre in L:
+                for centre in N:
                     #Il faut trouver une distance
                     T.append('''distance (individu, centre)''')
                     classe=T.index(min(T))
-                    L[classe].append(individu)#On place les individu dans les classes associés aux centre duquel ils sont les plus proches
+                    N[classe].append(individu)#On place les individu dans les classes associés aux centre duquel ils sont les plus proches
                     
             #On calcule maintenant les nouveaux centres
             for k in range (0, len(L)):
@@ -39,7 +41,7 @@ class Kmeans:
                     moyenne+=L[k][j]
                 moyenne=moyenne/len(L[k])
                 M.append(moyenne)
-                S.append(L[k]-M[k])
+                S.append(L[k][0]-M[k])
             
             
                     
