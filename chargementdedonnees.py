7d09ca118a9958c793a27658c4cc6d48f8047bd9
Julie-Bouchard <82224197+Julie-Bouchard@users.noreply.github.com>
2021-05-03 12:15:51 +0200

    Update chargementdedonnees.py

--- chargementdedonnees.py
+++ chargementdedonnees.py
@@ -12,6 +12,13 @@ class ChargementDeDonnees(Transformation):
     def __init__(self,emplacement_fichier):
         self.emplacement_fichier=emplacement_fichier
     def chargementdedonnees(self):
-        with open(self.emplacement_fichier, newline='') as csvfile:
-            fichier = csv.reader(csvfile, delimiter=' ', quotechar='|')
-            print(fichier)
+        #Exemple d'emplacement_fichier : 'C:/Users/Julie/Documents/1A/Projet de traitement de données/Données Hospitalisations Covid-19 en France-20210503/Données/covid-hospit-incid-reg-2021-03-03-17h20.csv'
+        #Le tableau sera une liste de listes
+        Tableau = []
+        #ouverture et lecture du fichier csv
+        f = open(self.emplacement_fichier)
+        csv_f = csv.reader(f)
+        for row in csv_f:
+            Tableau.append(row)
+        f.close
+        return (Tableau)
