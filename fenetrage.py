bb21bd97e61ac8c23c8bd09175befa0a32578545
Julie-Bouchard <82224197+Julie-Bouchard@users.noreply.github.com>
2021-05-06 14:24:02 +0200

    Add files via upload

---
+++ fenetrage.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  6 13:59:57 2021
+
+@author: id1776
+"""
+
+from transformation import Transformation
+
+class Fenetrage(Transformation):
+    def __init__(self,debut_periode,fin_periode):
+        self.debut_periode=debut_periode
+        self.fin_periode=fin_periode
+    def fenetrage(self):
+        fenetre=[]
+        for j in len(Donnees[1]):
+            if Donnees[1][j]=="jour":
+                for i in len(Donnees):
+                    if Donnees[i][j]==debut_periode:
+                        fenetre.append(Donnees[i])
+        return(fenetre)
