c28573e16adfea9ca7636716fff0e9e7952082b7
Julie-Bouchard <82224197+Julie-Bouchard@users.noreply.github.com>
2021-05-07 15:57:49 +0200

    Update testchargementdedonnees.py

--- testchargementdedonnees.py
+++ testchargementdedonnees.py
@@ -6,8 +6,9 @@ Created on Thu May  6 14:44:30 2021
 """
 
 import csv
+import json
 
-def chargementdedonnees(emplacement_fichier):
+def chargementcsv(emplacement_fichier):
         #Exemple d'emplacement_fichier : 'C:/Users/Julie/Documents/1A/Projet de traitement de données/Données Hospitalisations Covid-19 en France-20210503/Données/covid-hospit-incid-reg-2021-03-03-17h20.csv'
         Tableau = []
         #Ouverture et lecture du fichier csv
@@ -25,5 +26,13 @@ def chargementdedonnees(emplacement_fichier):
             Tableau_res.append(t)
         return (Tableau_res)
         
-Tableau1=chargementdedonnees("P:/ProjetTraitement/Données Hospitalisations Covid-19 en France-20210506/Données/covid-hospit-incid-reg-2021-03-03-17h20.csv")
+Tableau1=chargementcsv("P:/ProjetTraitement/Données Hospitalisations Covid-19 en France-20210506/Données/donnees-hospitalieres-nouveaux-covid19-2021-03-03-17h03.csv")
 print(Tableau1)
+
+def chargementjson(emplacement_fichier):
+        with open(emplacement_fichier) as json_file :
+            data = json.load(json_file)
+        return(data)
+    
+Tableau2=chargementjson("P:/ProjetTraitement/VacancesScolaires.json")
+print(Tableau2)
