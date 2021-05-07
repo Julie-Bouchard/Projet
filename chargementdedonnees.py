03433ac0d6a6bb4a0cccde88def6b52ecf4ada2a
Julie-Bouchard <82224197+Julie-Bouchard@users.noreply.github.com>
2021-05-07 15:56:19 +0200

    Update chargementdedonnees.py

--- chargementdedonnees.py
+++ chargementdedonnees.py
@@ -7,11 +7,12 @@ Created on Fri Apr 23 10:43:11 2021
 
 from transformation import Transformation
 import csv
+import json
 
 class ChargementDeDonnees(Transformation):
     def __init__(self,emplacement_fichier):
         self.emplacement_fichier=emplacement_fichier
-    def chargementdedonnees(self):
+    def chargementcsv(self):
         #Exemple d'emplacement_fichier : 'C:/Users/Julie/Documents/1A/Projet de traitement de données/Données Hospitalisations Covid-19 en France-20210503/Données/covid-hospit-incid-reg-2021-03-03-17h20.csv'
         Tableau = []
         #Ouverture et lecture du fichier csv
@@ -28,3 +29,9 @@ class ChargementDeDonnees(Transformation):
             t=Tableau[i].split(";")
             Tableau_res.append(t)
         return (Tableau_res)
+    def chargementjson(self):
+        # Dossier où se trouve le fichier  :
+        folder = ""
+        filename = "VacancesScolaires.json"
+        with open(folder + filename) as json_file :
+            data = json.load(json_file)
