a1a751d3f0cbe460563583798179d29a2872811a
Julie-Bouchard <82224197+Julie-Bouchard@users.noreply.github.com>
2021-05-08 20:58:24 +0200

    Update fenetrage.py

--- fenetrage.py
+++ fenetrage.py
@@ -6,21 +6,23 @@ Created on Thu May  6 13:59:57 2021
 """
 
 from transformation import Transformation
+from datetime import datetime
+
+def convertirdate(datestr):
+        #Exemple de datestr : "2020-03-19"
+        return datetime.strptime(datestr,"%Y-%m-%d")
 
 class Fenetrage(Transformation):
     def __init__(self,debut_periode,fin_periode):
         self.debut_periode=debut_periode
         self.fin_periode=fin_periode
     def fenetrage(self):
-        fenetre=[]
-        for j in range(len(Donnees[0])):
-            if Donnees[0][j]=="jour":
-                for i in range(len(Donnees)):
-                    if Donnees[i][j]==debut_periode:
-                        fenetre.append(Donnees[i])
-                        k=i
-                while Donnees[k][j]!=fin_periode:
-                    fenetre.append(Donnees[k])
-                    k+=1
-                fenetre.append(Donnees[k+1][j])
+        fenetre=[self.Donnees[0]]
+        d=convertirdate(self.debut_periode)
+        f=convertirdate(self.fin_periode)
+        for j in range(len(self.Donnees[0])):
+            if self.Donnees[0][j]=="jour":
+                for i in range(1,len(self.Donnees)):
+                    if convertirdate(self.Donnees[i][j])>=d and convertirdate(self.Donnees[i][j])<=f:
+                        fenetre.append(self.Donnees[i])
         return(fenetre)
