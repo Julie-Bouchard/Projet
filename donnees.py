f299a155fee03005cbbcb946140d528c6309de2b
Julie-Bouchard <82224197+Julie-Bouchard@users.noreply.github.com>
2021-05-11 14:18:03 +0200

    Update donnees.py

--- donnees.py
+++ donnees.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 23 12:03:42 2021
-
-@author: Julie
+Created on Mon May 10 15:37:32 2021
+@author: id1776
 """
 
-class Donnees:
-    def __init__(self,nombre_lignes,nombre_colonnes):
-        self.nombre_lignes=nombre_lignes
-        self.nombre_colonnes=nombre_colonnes
-       
-    def donnees(self):
-        return [[0]*self.nombre_colonnes for i in range(self.nombre_lignes)]
+class Donnees():
+    def __init__(self,noms_colonnes,lignes):
+        self.noms_colonnes=noms_colonnes
+        self.lignes=lignes
+    def ajouter_ligne(self,ligne, position = -1):
+        self.lignes.insert(position,ligne)
+    def enlever_ligne(self,position):
+        del(self.lignes[position])
+    def ajouter_colonne(self,nom, colonne, position = -1):
+        self.noms_colonnes.insert(position,nom)
+        for i in range(len(self.lignes)):
+            self.lignes[i].insert(position,colonne[i])
+    def enlever_colonne(self,position=-1):
+        for i in range(len(self.lignes)):
+            del(self.lignes[i][position])
