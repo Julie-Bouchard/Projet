e0f9a31d05bcccfedc22a1779bb81ec3144cc56b
Julie-Bouchard <82224197+Julie-Bouchard@users.noreply.github.com>
2021-05-11 18:26:48 +0200

    Update fenetrage.py

--- fenetrage.py
+++ fenetrage.py
@@ -6,6 +6,7 @@ Created on Thu May  6 13:59:57 2021
 """
 
 from transformation import Transformation
+from donnees import Donnees
 from datetime import datetime
 
 def convertirdate(datestr):
@@ -13,16 +14,18 @@ def convertirdate(datestr):
         return datetime.strptime(datestr,"%Y-%m-%d")
 
 class Fenetrage(Transformation):
-    def __init__(self,debut_periode,fin_periode):
+    def __init__(self,table,debut_periode,fin_periode):
         self.debut_periode=debut_periode
         self.fin_periode=fin_periode
-    def fenetrage(self):
-        fenetre=[self.Donnees[0]]
+        self.table=table
+    def transforme(self):
+        fenetre=[self.table.noms_colonnes]
         d=convertirdate(self.debut_periode)
         f=convertirdate(self.fin_periode)
-        for j in range(len(self.Donnees[0])):
-            if self.Donnees[0][j]=="jour":
-                for i in range(1,len(self.Donnees)):
-                    if convertirdate(self.Donnees[i][j])>=d and convertirdate(self.Donnees[i][j])<=f:
-                        fenetre.append(self.Donnees[i])
-        return(fenetre)
+        for j in range(len(self.table.noms_colonnes)):
+            if self.table.noms_colonnes[j]=="jour":
+                for i in range(1,len(self.table.lignes)+1):
+                    if convertirdate(self.table.lignes[i-1][j])>=d and convertirdate(self.table.lignes[i-1][j])<=f:
+                        fenetre.append(self.table.lignes[i-1])
+        data=Donnees(fenetre[0],fenetre[1:])
+        return(data)
