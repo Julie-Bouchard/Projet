d5535f8ebba5b0609b874ef720c714f33c1a91b1
Julie-Bouchard <82224197+Julie-Bouchard@users.noreply.github.com>
2021-05-11 15:06:30 +0200

    Update testclasses.py

--- testclasses.py
+++ testclasses.py
@@ -5,9 +5,28 @@ Created on Mon May 10 14:57:31 2021
 @author: id1776
 """
 
-from donnees1 import Donnees
-from chargementdedonnees import ChargementDeDonnees
+from donnees import Donnees
+from transformation import Transformation
 
-a=ChargementDeDonnees("P:/ProjetTraitement/Données Hospitalisations Covid-19 en France-20210506/Données/donnees-hospitalieres-nouveaux-covid19-2021-03-03-17h03.csv")
-table=ChargementDeDonnees.chargementcsv(a)
-print(table.lignes)
+a=ChargementDeDonneescsv("P:/ProjetTraitement/Données Hospitalisations Covid-19 en France-20210506/Données/donnees-hospitalieres-nouveaux-covid19-2021-03-03-17h03.csv")
+table=ChargementDeDonneescsv.transforme(a)
+
+a2=ChargementDeDonneescsv("P:/ProjetTraitement/Données Hospitalisations Covid-19 en France-20210506/Données/donnees-hospitalieres-covid19-2021-03-03-17h03.csv")
+tablea=ChargementDeDonneescsv.transforme(a2)
+
+b=Fenetrage(table,"2020-03-19","2020-03-20")
+table2=Fenetrage.transforme(b)
+print(table2.lignes)
+print(table2.noms_colonnes)
+
+c=SelectionVariablecsv(table,'dep')
+table3=SelectionVariablecsv.transforme(c)
+print(table3.lignes)
+
+d=AgregationSpatiale_reg_dep(table,'dep')
+table4=AgregationSpatiale_reg_dep.transforme(d)
+print(table4.lignes)
+
+e=Jointure(table,tablea,'dep')
+table5=Jointure.transforme(e)
+print(table5.lignes)
