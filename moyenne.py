c879e3ae3a2113737c408269f17c88fcf630861f
Julie-Bouchard <82224197+Julie-Bouchard@users.noreply.github.com>
2021-05-11 13:23:09 +0200

    Update moyenne.py

--- moyenne.py
+++ moyenne.py
@@ -7,5 +7,12 @@ Created on Fri Apr 23 11:56:31 2021
 
 from estimateur import Estimateur
 
-class Moyenne:
+class Moyenne(Estimateur):
+    #self represente un vecteur
     def __init__(self):
+        self.moyenne=0
+    def estime(self):    
+        n= len(self)
+        for i in range (0,n):
+            self.moyenne+=self[i,0]
+        self.moyenne=self.moyenne / n
