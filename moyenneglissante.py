4be0f4b0daf906d9180af5643efba24f219680cb
Julie-Bouchard <82224197+Julie-Bouchard@users.noreply.github.com>
2021-05-11 18:19:13 +0200

    Update moyenneglissante.py

--- moyenneglissante.py
+++ moyenneglissante.py
@@ -8,7 +8,7 @@ Created on Tue May 11 01:28:58 2021
 from transformation import Transformation
 from estimateur import Estimateur
 from donnees import Donnees
-from datetime import datetime
+from datetime import timedelta
 
 class MoyenneGlissante(Transformation):
     def __init__(self,table,nom_variable,periode):
@@ -19,20 +19,32 @@ class MoyenneGlissante(Transformation):
         mg=[]
         j=self.table.noms_colonnes.index("jour")
         v=self.table.noms_colonnes.index(self.nom_variable)
-        for i in range(len(self.table.lignes)+1):
+        for i in range(1,len(self.table.lignes)+1):
             #Si la date est proche du début ou de la fin des données, on met une valeur manquante
-            if convertirdate(self.table.lignes[i][j]) - datetime.timedelta((self.periode-1)/2) < convertirdate(self.table.lignes[0][j]) or convertirdate(self.table.lignes[i][j]) + datetime.timedelta((self.periode-1)/2) > convertirdate(self.table.lignes[-1][j]):
+            if convertirdate(self.table.lignes[i][j]) - timedelta((self.periode-1)/2) < convertirdate(self.table.lignes[0][j]) or convertirdate(self.table.lignes[i][j]) + timedelta((self.periode-1)/2) > convertirdate(self.table.lignes[-1][j]):
                 mg.insert(i,None)
             else:
                 donneesperiode=[[self.nom_variable]]
                 #On compare les valeurs des autres variables
-                l=self.table.noms_colonnes.index("dep") or l=self.table.noms_colonnes.index("numReg") or l=self.table.noms_colonnes.index("reg")
-                m=self.table.noms_colonnes.index("sexe") or n=self.table.noms_colonnes.index("cl_age90")
-                for k in range(len(self.table.lignes)+1):
+                if "dep" in self.table.noms_colonnes:
+                    l=self.table.noms_colonnes.index("dep") 
+                if "numReg" in self.table.noms_colonnes:
+                    l=self.table.noms_colonnes.index("numReg")
+                if "reg" in self.table.noms_colonnes:
+                    l=self.table.noms_colonnes.index("reg")
+                if "sexe" in self.table.noms_colonnes:
+                    m=self.table.noms_colonnes.index("sexe")
+                if "cl_age90" in self.table.noms_colonnes:
+                    m=self.table.noms_colonnes.index("cl_age90")
+                for k in range(1,len(self.table.lignes)+1):
                     #Pour faire la moyenne, on conserve seulement les valeurs du même département, de la même région, du même sexe, de la même classe d'âge
                     #On prend les valeurs qui sont dans la bonne période
-                    if self.table.lignes[k][l]==self.table.lignes[i][l] and self.table.lignes[k][m]==self.table.lignes[i][m] and convertirdate(self.table.lignes[k][j]) >= convertirdate(self.table.lignes[i][j]) - datetime.timedelta((self.periode-1)/2) and convertirdate(self.table.lignes[k][j]) <= convertirdate(self.table.lignes[i][j]) + datetime.timedelta((self.periode-1)/2):
-                        donneesperiode.append(self.table.lignes[k][v])
+                    if "sexe" in self.table.noms_colonnes or "cl_age90" in self.table.noms_colonnes:
+                        if self.table.lignes[k-1][l]==self.table.lignes[i-1][l] and self.table.lignes[k-1][m]==self.table.lignes[i-1][m] and convertirdate(self.table.lignes[k-1][j]) >= convertirdate(self.table.lignes[i-1][j]) - timedelta((self.periode-1)/2) and convertirdate(self.table.lignes[k-1][j]) <= convertirdate(self.table.lignes[i-1][j]) + timedelta((self.periode-1)/2):
+                            donneesperiode.append(self.table.lignes[k-1][v])
+                    else:
+                        if self.table.lignes[k-1][l]==self.table.lignes[i-1][l] and convertirdate(self.table.lignes[k-1][j]) >= convertirdate(self.table.lignes[i-1][j]) - timedelta((self.periode-1)/2) and convertirdate(self.table.lignes[k-1][j]) <= convertirdate(self.table.lignes[i-1][j]) + timedelta((self.periode-1)/2):
+                            donneesperiode.append(self.table.lignes[k-1][v])
                 dataperiode=Donnees(donneesperiode[0],donneesperiode[1:])
                 mg.insert(i,Moyenne.estime(dataperiode))
         res=Donnees.ajouter_colonne("moyenne glissante" + self.nom_variable, mg, position = -1)
