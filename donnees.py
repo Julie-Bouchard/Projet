a5fa89db3fc8f726350eeed358e7439754a03b35
Julie-Bouchard <82224197+Julie-Bouchard@users.noreply.github.com>
2021-05-12 11:19:43 +0200

    Update donnees.py

--- donnees.py
+++ donnees.py
@@ -9,13 +9,26 @@ class Donnees():
         self.noms_colonnes=noms_colonnes
         self.lignes=lignes
     def ajouter_ligne(self,ligne, position = -1):
-        self.lignes.insert(position,ligne)
-    def enlever_ligne(self,position):
+        if position==-1:
+            self.lignes.append(ligne)
+        else:
+            self.lignes.insert(position,ligne)
+        return Donnees(self.noms_colonnes,self.lignes)
+    def enlever_ligne(self,position=-1):
         del(self.lignes[position])
+        return Donnees(self.noms_colonnes,self.lignes)
     def ajouter_colonne(self,nom, colonne, position = -1):
-        self.noms_colonnes.insert(position,nom)
-        for i in range(len(self.lignes)):
-            self.lignes[i].insert(position,colonne[i])
+        if position==-1:
+            self.noms_colonnes.append(nom)
+            for i in range(len(self.lignes)):
+                self.lignes[i].append(colonne[i])
+        else:
+            self.noms_colonnes.insert(position,nom)
+            for i in range(len(self.lignes)):
+                self.lignes[i].insert(position,colonne[i])
+        return Donnees(self.noms_colonnes,self.lignes)
     def enlever_colonne(self,position=-1):
+        del(self.noms_colonnes[position])
         for i in range(len(self.lignes)):
             del(self.lignes[i][position])
+        return Donnees(self.noms_colonnes,self.lignes)
