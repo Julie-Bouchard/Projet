5df30b0a26791af6494e6cee6d758cf03fafba8a
Julie-Bouchard <82224197+Julie-Bouchard@users.noreply.github.com>
2021-05-12 11:26:18 +0200

    Update moyenneglissante.py

--- moyenneglissante.py
+++ moyenneglissante.py
@@ -7,6 +7,7 @@ Created on Tue May 11 01:28:58 2021
 
 from transformation import Transformation
 from estimateur import Estimateur
+from moyenne import Moyenne
 from donnees import Donnees
 from datetime import timedelta
 
@@ -21,7 +22,7 @@ class MoyenneGlissante(Transformation):
         v=self.table.noms_colonnes.index(self.nom_variable)
         for i in range(1,len(self.table.lignes)+1):
             #Si la date est proche du début ou de la fin des données, on met une valeur manquante
-            if convertirdate(self.table.lignes[i][j]) - timedelta((self.periode-1)/2) < convertirdate(self.table.lignes[0][j]) or convertirdate(self.table.lignes[i][j]) + timedelta((self.periode-1)/2) > convertirdate(self.table.lignes[-1][j]):
+            if convertirdate(self.table.lignes[i-1][j]) - timedelta((self.periode-1)/2) < convertirdate(self.table.lignes[0][j]) or convertirdate(self.table.lignes[i-1][j]) + timedelta((self.periode-1)/2) > convertirdate(self.table.lignes[-1][j]):
                 mg.insert(i,None)
             else:
                 donneesperiode=[[self.nom_variable]]
@@ -46,6 +47,9 @@ class MoyenneGlissante(Transformation):
                         if self.table.lignes[k-1][l]==self.table.lignes[i-1][l] and convertirdate(self.table.lignes[k-1][j]) >= convertirdate(self.table.lignes[i-1][j]) - timedelta((self.periode-1)/2) and convertirdate(self.table.lignes[k-1][j]) <= convertirdate(self.table.lignes[i-1][j]) + timedelta((self.periode-1)/2):
                             donneesperiode.append(self.table.lignes[k-1][v])
                 dataperiode=Donnees(donneesperiode[0],donneesperiode[1:])
-                mg.insert(i,Moyenne.estime(dataperiode))
-        res=Donnees.ajouter_colonne("moyenne glissante" + self.nom_variable, mg, position = -1)
+                print(dataperiode.lignes)
+                moyenne=Moyenne(dataperiode)
+                mg.insert(i,Moyenne.estime(moyenne))
+                print(Moyenne.estime(moyenne)
+        res=Donnees.ajouter_colonne(self.table, "moyenne glissante " + self.nom_variable, mg, position = -1)
         return res
