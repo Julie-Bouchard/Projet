cff06906b05d3a0d553b13acc129d658cc3ae777
anaiscolson <83553017+anaiscolson@users.noreply.github.com>
2021-05-12 05:40:28 +0200

    Update tableau.py

--- tableau.py
+++ tableau.py
@@ -14,6 +14,19 @@ class Tableau(Affichage):
 
     def SauvegarderTableauEstimateur(estimateur, fenetrage):
         self.table = estimateur.estime(fenetrage)
+        with open('tableur.csv','w',newline='') as f :
+            ecrire=csv.writter(f)
+            for i in self.table :
+                ecrire.writerow(i)
+                print('',end='\n')
+                print(longueur du tableau :', len(self.table))
         
     def SauvegarderTableauTransformation(transformation, fenetrage):
         self.table = transformation.transforme(fenetrage)
+        with open('tableur.csv','w',newline='') as f :
+            ecrire=csv.writter(f)
+            for i in self.table :
+                ecrire.writerow(i)
+                print('',end='\n')
+                print(longueur du tableau :', len(self.table))
+                
