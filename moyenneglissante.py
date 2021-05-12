34e0e619bc04ba2284c42d31e0cec275a70fc18d
Julie-Bouchard <82224197+Julie-Bouchard@users.noreply.github.com>
2021-05-12 19:18:19 +0200

    Update moyenneglissante.py

--- moyenneglissante.py
+++ moyenneglissante.py
@@ -23,16 +23,16 @@ class MoyenneGlissante(Transformation):
         for i in range(1,len(self.table.lignes)+1):
             #Si la date est proche du début ou de la fin des données, on met une valeur manquante
             if convertirdate(self.table.lignes[i-1][j]) - timedelta((self.periode-1)/2) < convertirdate(self.table.lignes[0][j]) or convertirdate(self.table.lignes[i-1][j]) + timedelta((self.periode-1)/2) > convertirdate(self.table.lignes[-1][j]):
-                mg.insert(i,None)
+                mg.insert(i-1,'None')
             else:
                 donneesperiode=[[self.nom_variable]]
                 #On compare les valeurs des autres variables
                 if "dep" in self.table.noms_colonnes:
-                    l=self.table.noms_colonnes.index("dep") 
+                    lieu=self.table.noms_colonnes.index("dep") 
                 if "numReg" in self.table.noms_colonnes:
-                    l=self.table.noms_colonnes.index("numReg")
+                    lieu=self.table.noms_colonnes.index("numReg")
                 if "reg" in self.table.noms_colonnes:
-                    l=self.table.noms_colonnes.index("reg")
+                    lieu=self.table.noms_colonnes.index("reg")
                 if "sexe" in self.table.noms_colonnes:
                     m=self.table.noms_colonnes.index("sexe")
                 if "cl_age90" in self.table.noms_colonnes:
@@ -41,15 +41,14 @@ class MoyenneGlissante(Transformation):
                     #Pour faire la moyenne, on conserve seulement les valeurs du même département, de la même région, du même sexe, de la même classe d'âge
                     #On prend les valeurs qui sont dans la bonne période
                     if "sexe" in self.table.noms_colonnes or "cl_age90" in self.table.noms_colonnes:
-                        if self.table.lignes[k-1][l]==self.table.lignes[i-1][l] and self.table.lignes[k-1][m]==self.table.lignes[i-1][m] and convertirdate(self.table.lignes[k-1][j]) >= convertirdate(self.table.lignes[i-1][j]) - timedelta((self.periode-1)/2) and convertirdate(self.table.lignes[k-1][j]) <= convertirdate(self.table.lignes[i-1][j]) + timedelta((self.periode-1)/2):
-                            donneesperiode.append(self.table.lignes[k-1][v])
+                        if self.table.lignes[k-1][lieu]==self.table.lignes[i-1][lieu] and self.table.lignes[k-1][m]==self.table.lignes[i-1][m] and convertirdate(self.table.lignes[k-1][j]) >= convertirdate(self.table.lignes[i-1][j]) - timedelta((self.periode-1)/2) and convertirdate(self.table.lignes[k-1][j]) <= convertirdate(self.table.lignes[i-1][j]) + timedelta((self.periode-1)/2):
+                            donneesperiode.append([self.table.lignes[k-1][v]])
                     else:
-                        if self.table.lignes[k-1][l]==self.table.lignes[i-1][l] and convertirdate(self.table.lignes[k-1][j]) >= convertirdate(self.table.lignes[i-1][j]) - timedelta((self.periode-1)/2) and convertirdate(self.table.lignes[k-1][j]) <= convertirdate(self.table.lignes[i-1][j]) + timedelta((self.periode-1)/2):
-                            donneesperiode.append(self.table.lignes[k-1][v])
+                        if self.table.lignes[k-1][lieu]==self.table.lignes[i-1][lieu] and convertirdate(self.table.lignes[k-1][j]) >= convertirdate(self.table.lignes[i-1][j]) - timedelta((self.periode-1)/2) and convertirdate(self.table.lignes[k-1][j]) <= convertirdate(self.table.lignes[i-1][j]) + timedelta((self.periode-1)/2):
+                            donneesperiode.append([self.table.lignes[k-1][v]])
                 dataperiode=Donnees(donneesperiode[0],donneesperiode[1:])
-                print(dataperiode.lignes)
-                moyenne=Moyenne(dataperiode)
-                mg.insert(i,Moyenne.estime(moyenne))
-                print(Moyenne.estime(moyenne)
-        res=Donnees.ajouter_colonne(self.table, "moyenne glissante " + self.nom_variable, mg, position = -1)
+                mdata=Moyenne(dataperiode)
+                mean=Moyenne.estime(mdata)
+                mg.append(str(mean))
+        res=Donnees.ajouter_colonne(self.table, "moyenneglissante", mg)
         return res
