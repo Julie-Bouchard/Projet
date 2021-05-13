2504bf4b59ee4ebc8ced35ab7c4128664126f0f2
MaroineMrc <83315095+MaroineMrc@users.noreply.github.com>
2021-05-13 22:04:37 +0200

    Update Kmeans.py

--- Kmeans.py
+++ Kmeans.py
@@ -1,69 +1,59 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May  1 00:35:22 2021
-
-@author: maroi
-"""
-import numpy as np
+import random
+from donnees import Donnees
+from estimateur import Estimateur
+from normalisation import Normalisation
+from outil import Outil
 
 
-class Kmeans:
-    def __init__(self, nombre_classe):
-        self.nombre_classe=nombre_classe
-        
-    def produit_scalaire(U,V):
-        #U, V deux vecteurs de même taille
-        n,p=U.shape
-        P=0 #Vecteur P=U-V
-        for i in range (0,n):
-            P+=U[i]*V[i]    
-        return P[0]
-            
-    def norme(U):
-        norm=(produit_scalaire(U,U))**(1/2)
-        return norm
-        
-    def distance(U,V):
-        S=U-V
-        distance=norme(S)
-        return distance
+class Kmeans(Estimateur):
     
-    def algorithme(self):
-        n,p=self.shape
-        #On considère que notre table est sous forme de matrice.
-        #Disons une matrice de taille n,p. Il y'a donc n individus et p variables.
-        #La première etape consite à initialiser l'algorithme et désigner des centres au hasard
-        #On selectionne les k premiers individus de la table.
-        L=[] #La liste des centres mobiles à l'étape 0
-        for k in range (0, self.nombre_classe):
-            L.append([self[k]])
-        M=L
-        S=L
-        i=0
-        #Une fois que l'initialisation est faite il faut faire bouger les centres jusqu'à avoir une partition stable.
-        while norme(S[k])>10**(-2) and i<50 :
-            i+=1
-            N=M #La liste des centres mobiles à l'étape n
-            M=[] #La liste des centres mobiles à l'étape n+1
-            S=[] #La liste des différence entre l'étape n et n+1
-            #On partitionne maintenant notre table
-            #On calcule toute les distances
-            for i in range (0,n) :
+
+    def __init__(self,nombre_classe,colonnes):
+        self.nombre_classe = nombre_classe
+        self.colonnes = colonnes #Liste des colonnes qui vont être utilisé pour le kmeans
+        
+
+    def estime(self,table):
+        New_Table=Outil.changement_ordre(table,self.colonnes)
+        nombre_variable=len(self.colonnes)
+        k=self.nombre_classe
+        
+        for nom in self.colonnes :
+            New_Table=Outil.convertisseur_str_float(New_Table,nom)
+        N=Normalisation(self.colonnes)
+        N.transforme(New_Table)
+        n=len(New_Table.lignes)
+        indice_classe=New_Table.noms_colonnes.index('Classes')
+        #centres=[]
+        #for i in range(k):
+        #    centres.append(New_Table.lignes[i][1:(nombre_variable+1)])
+        
+        centres=[[int(random.random()*10) for j in range(nombre_variable)] for i in range(k)]
+        classes=Outil.extrait_colonne(New_Table,'Classes')[1]
+        classes2=[]
+        for _ in range(len(New_Table.lignes)):
+            classes2.append(-1)
+
+
+        iteration=0
+        while classes != classes2 and iteration<200:
+            '''nb est un cap arbitraire pas trop grand pour éviter les boucles infinies si une observation oscille'''
+            iteration+=1
+            for s in range(len(New_Table.lignes)):
+                classes2[s]=classes[s]
+            #classes2=[[classes[k]] for k in range(len(New_Table.lignes))]
+            for j in range (n):
                 T=[] #La liste des distance de cet individu avec les centres
-                for centre in N:
-                    #Il faut trouver une distance
-                    T.append(distance (self[i], centre))
-                classe=T.index(min(T))
-                N[classe].append(self[i])#On place les individu dans les classes associés aux centre duquel ils sont les plus proches
-                #A ce moment de l'algorithme, la partition est faite    
-            #On calcule maintenant les nouveaux centres
-            for k in range (0, len(N)):
-                moyenne=0
-                for j in range (0, len (N[k])):
-                    moyenne+=N[k][j]
-                moyenne=moyenne/len(N[k])
-                M.append(moyenne)
-                S.append(N[k][0]-M[k])
-            
-            
-                    
+                for centre in centres:
+                    T.append(Outil.distance(New_Table.lignes[j][1:], centre,nombre_variable))
+                New_Table.lignes[j][indice_classe] = T.index(min(T))
+                
+            Outil.calcul_centres(New_Table,centres,k,nombre_variable,indice_classe)
+            classes=Outil.extrait_colonne(New_Table,'Classes')[1]
+        #print(New_Table.noms_colonnes)
+        #print(New_Table.lignes)
+        #table.ajouter_colonne('Classes',classes)
+        print(classes)
+        return New_Table
+
+
