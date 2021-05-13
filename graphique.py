efdff6718641acfe3402e448463f152451c6a814
anaiscolson <83553017+anaiscolson@users.noreply.github.com>
2021-05-13 19:20:01 +0200

    Update graphique.py

--- graphique.py
+++ graphique.py
@@ -12,21 +12,20 @@ import numpy as np
 
 class Graphique(Affichage):
     
-    def __init__(self, titre, abscisse, ordonnee, table):
-        self.titre = titre
-        self.abscisse = abscisse
-        self.ordonnee = ordonnee
-        self.table = table
+    def __init__(self, titre, abscisse, ordonnee):
+        self.titre = titre # de type str, il correspond au titre que l'on veut donner au graphique
+        self.abscisse = abscisse # de type str, il correspond au nom de la variable que l'on veut mettre en abscisse sur le graphe
+        self.ordonnee = ordonnee # de type str, il correspond au nom de la variable que l'on veut mettre en ordonnee sur le graphe
     
-    def affiche(self):
-        x = []
-        y = []
-        indice_colonne_x = self.table.noms_colonnes.index(self.abscisse)
-        indice_colonne_y = self.table.noms_colonnes.index(self.ordonnee)
-        n = len(self.table.lignes) 
+    def affiche(self, table):
+        x = [] # liste des abscisses du graphique
+        y = [] # liste des ordonnées du graphique
+        indice_colonne_x = table.noms_colonnes.index(self.abscisse) # numero de la colonne associee à la variable que l'ont veut en abscisse
+        indice_colonne_y = table.noms_colonnes.index(self.ordonnee) # numero de la colonne associee à la variable que l'ont veut en ordonnee
+        n = len(table.lignes) 
         for i in range(n) :
-            x.append(self.table.lignes[i][indice_colonne_x])
-            y.append(self.table.lignes[i][indice_colonne_y])
+            x.append(table.lignes[i][indice_colonne_x])
+            y.append(table.lignes[i][indice_colonne_y])
         plt.ylabel(self.ordonnee)
         plt.xlabel(self.abscisse)
         plt.title(self.titre)
