b8c46396da544ccac23160ad26a3b503dc90845e
MaroineMrc <83315095+MaroineMrc@users.noreply.github.com>
2021-05-13 18:36:09 +0200

    Update Normalisation.py

--- Normalisation.py
+++ Normalisation.py
@@ -1,18 +1,27 @@
-import Statistique_descriptive as sd
+from operation import Operation
+from estimateur import Estimateur
+from ecarttype import EcartType
+from selectionvariablecsv import SelectionVariablecsv
+from centrage import Centrage
 
 class Normalisation:
-    def __init__(self):
-        self.normalisee="False"
+    def __init__(self, Liste_variable):
+        self.Liste_variable=Liste_variable
         
-    def normaliser(self):
-        n=len(self)
-        p='''nombre de variables'''
+    def transforme(self,table):
+        
+        table=Centrage(self.Liste_variable).transforme(table)
+        n=len(table.lignes)+1
+        p=len(table.noms_colonnes)
         for j in range (0,p):
-            U=[individu[j] for individu in self] # On selectionne la j ème colonne de notre base de donnée
-            ecart_type=sd.U.ecart_type
-            for i in range (0,n):
-                self[i][j]=self[i][j]/ecart_type
-        self.normalisee="True"
+            if table.noms_colonnes[j] in self.Liste_variable:
+                colonne=SelectionVariablecsv(table.noms_colonnes[j]).transforme(table)# On selectionne la j ème colonne de notre base de donnée
+                sd=EcartType().estime(colonne)
+                for i in range (1,n):
+                    table.lignes[i-1][j]=float(table.lignes[i-1][j])/float(sd.lignes[0][0])
+        
+        return table
+     
         
         
         
