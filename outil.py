4980ef89e54f89092e20b0a0aa51821f1f3b76b4
MaroineMrc <83315095+MaroineMrc@users.noreply.github.com>
2021-05-13 22:05:06 +0200

    Update outil.py

--- outil.py
+++ outil.py
@@ -1,6 +1,7 @@
 from chargementdedonneesjson import ChargementDeDonneesjson
 from operation import Operation
 from datetime import datetime
+import random
 
 
 class Outil(Operation):
@@ -25,4 +26,52 @@ class Outil(Operation):
         #Exemple de datestr : "2020-03-19"
         return datetime.strptime(datestr,"%Y-%m-%d")
     
+    def calcul_centres(table,centres,nombre_classe,nombre_variable,colonne_classe):
+        for i in range(nombre_classe):
+            population = 0
+            new_centre=[0 for m in range(nombre_variable)]
+            for q in range(len(table.lignes)):
+                if table.lignes[q][colonne_classe] == i :
+                    population += 1
+                    for p in range(nombre_variable):
+                        new_centre[p]+=table.lignes[q][p]
+            for j in range(nombre_variable):
+                if population != 0 :
+                    new_centre[p]=new_centre[p]/population
+                if population == 0:
+                    new_centre = [int(random.random()*10) for j in range(nombre_variable)]
+            centres[i]=new_centre
+
+    def distance(X,Y,nombre_variable):
+        
+        distance=0
+        for i in range(nombre_variable):
+            distance+=(X[i]-Y[i])**2
+        return(distance**(1/2))
+
+    def extrait_colonne(table,nom_colonne):
+        indice=table.noms_colonnes.index(nom_colonne)
+        colonne_extraite=[]
+        for ligne in table.lignes:
+            colonne_extraite.append(ligne[indice])
+        return (nom_colonne,colonne_extraite)
+
+    
+    def changement_ordre(table,colonnes):
+        Table_bis=Donnees(['Classes'],[[0] for k in range(len(table.lignes))])
+        for nom in colonnes :
+            extrait=Kmeans.extrait_colonne(table,nom)
+            Table_bis.ajouter_colonne(extrait[0],extrait[1])
+           
+        for nom in table.noms_colonnes :
+            if nom not in colonnes :
+                extrait2=Kmeans.extrait_colonne(table,nom)
+                Table_bis.ajouter_colonne(extrait2[0],extrait2[1])
+        return(Table_bis)
+    
+    def convertisseur_str_float(table,nom_colonnes):
+        j=table.noms_colonnes.index(nom_colonnes)
+        for i in range(len(table.lignes)):
+            table.lignes[i][j]=float(table.lignes[i][j])
+        return table
     
