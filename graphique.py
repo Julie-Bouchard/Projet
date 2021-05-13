258b1e1e5b2780ca57a182dee9009b8d5969968a
anaiscolson <83553017+anaiscolson@users.noreply.github.com>
2021-05-13 15:26:08 +0200

    Update graphique.py

--- graphique.py
+++ graphique.py
@@ -4,26 +4,16 @@ import numpy as np
 
 class Graphique(Affichage):
     
-    def __init__(self, titre, abscisse, ordonnee):
+    def __init__(self, titre, abscisse, ordonnee, table):
         self.titre = titre
         self.abscisse = abscisse
         self.ordonnee = ordonnee
-        self.table = []
+        self.table = table
     
-    def GraphiqueEstimateur(estimateur, fenetrage):
-        self.table = estimateur.estime(fenetrage)
+    def affiche(self):
+        n = len(table)
         x = self.table[0]
-        y = self.table[1]
-        plt.ylabel(ordonnee)
-        plt.xlabel(abscisse)
-        plt.title(titre)
-        plt.plot(x,y,'b')
-        plt.show()
-    
-    def GraphiqueTransformation(transformation, fenetrage):
-        self.table = transformation.transforme(fenetrage)
-        x = self.table[0]
-        y = self.table[1]
+        y = [self.table[1]]
         plt.ylabel(ordonnee)
         plt.xlabel(abscisse)
         plt.title(titre)
