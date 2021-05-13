5a1b5fb1681b99ab3d42ad6d3756a058c57da76e
MaroineMrc <83315095+MaroineMrc@users.noreply.github.com>
2021-05-13 18:35:36 +0200

    Update Centrage.py

--- Centrage.py
+++ Centrage.py
@@ -1,15 +1,19 @@
-import Statistique_descriptive as sd
+from operation import Operation
+from estimateur import Estimateur
+from moyenne import Moyenne
+from selectionvariablecsv import SelectionVariablecsv 
 
 class Centrage:
-    def __init__(self):
-        self.centree="False"
-        
-    def centrer(self):
-        n=len(self)
-        p='''nombre de variable'''
-        for j in range (0,n):
-            U=[individu[j] for individu in self] # On selectionne la j ème colonne de notre base de donnée
-            moyenne=sd.U.moyenne
-            for i in range (0,n):
-                self[i][j]=self[i][j]-moyenne
-        self.centree="True"
+    def __init__(self,Liste_variable):
+        self.Liste_variable=Liste_variable
+    
+    def transforme(self,table):
+        n=len(table.lignes)+1
+        p=len(table.noms_colonnes)
+        for j in range (0,p):
+            if table.noms_colonnes[j] in self.Liste_variable:
+                colonne=SelectionVariablecsv(table.noms_colonnes[j]).transforme(table)# On selectionne la j ème colonne de notre base de donnée 
+                moyenne=Moyenne().estime(colonne)
+                for i in range (1,n):
+                    table.lignes[i-1][j]=float(table.lignes[i-1][j])-float(moyenne.lignes[0][0])
+        return table
